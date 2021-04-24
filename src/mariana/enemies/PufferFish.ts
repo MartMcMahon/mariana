eb6f818f4bbdc37aa642f19c2fc24a96bd2971b5
Simon Baumgardt-Wellander <simonbw@uw.edu>
2021-04-24 14:23:52 -0700

    asset types

---
+++ src/mariana/enemies/PufferFish.ts
@@ -0,0 +1,42 @@
+import { Body, Circle } from "p2";
+import { AnimatedSprite, Sprite, Texture } from "pixi.js";
+import { runInContext } from "vm";
+import img_jellyfish from "../../../resources/images/jellyfish.png";
+import img_jellyfish1 from "../../../resources/images/jellyfish_1.png";
+import img_jellyfish2 from "../../../resources/images/jellyfish_2.png";
+import BaseEntity from "../../core/entity/BaseEntity";
+import Entity, { GameSprite } from "../../core/entity/Entity";
+import { rUniform } from "../../core/util/Random";
+import { V2d } from "../../core/Vector";
+import { Diver } from "../Diver";
+
+export class PufferFish extends BaseEntity implements Entity {
+  sprite: AnimatedSprite & GameSprite;
+
+  constructor(position: V2d, radius: number = rUniform(0.4, 0.9)) {
+    super();
+
+    this.body = new Body({ mass: 0, collisionResponse: false });
+    this.body.addShape(new Circle({ radius }));
+    this.body.position = position;
+
+    this.sprite = AnimatedSprite.fromImages([img_jellyfish1, img_jellyfish2]);
+    this.sprite.animationSpeed = rUniform(0.9, 1.2);
+    this.sprite.autoUpdate = false;
+    this.sprite.width = radius * 2;
+    this.sprite.height = radius * 2;
+    this.sprite.anchor.set(0.5);
+    this.sprite.loop = true;
+    this.sprite.position.set(...position);
+
+    this.sprite.update(Math.random() * 1.0);
+  }
+
+  onRender(dt: number) {}
+
+  onBeginContact(other: Entity) {
+    if (other instanceof Diver) {
+      other.damage(20);
+    }
+  }
+}
