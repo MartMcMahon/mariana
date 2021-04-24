eafe0d401b725deed22359c9343d1e5283b742d4
Simon Baumgardt-Wellander <simonbw@uw.edu>
2021-04-24 14:22:03 -0700

    preload jellyfish images

--- src/mariana/preloader/preloadImages.ts
+++ src/mariana/preloader/preloadImages.ts
@@ -5,6 +5,8 @@ import img_diver from "../../../resources/images/diver.png";
 import img_diverLeft from "../../../resources/images/diver_left.png";
 import img_diverRight from "../../../resources/images/diver_right.png";
 import img_jellyfish from "../../../resources/images/jellyfish.png";
+import img_jellyfish1 from "../../../resources/images/jellyfish_1.png";
+import img_jellyfish2 from "../../../resources/images/jellyfish_2.png";
 
 // Returns the list of all
 export function getImagesToPreload(): Set<string> {
@@ -17,6 +19,8 @@ export function getImagesToPreload(): Set<string> {
     img_boat,
     img_bubble,
     img_jellyfish,
+    img_jellyfish1,
+    img_jellyfish2,
   ]);
 
   // Just in case this sneaks in there somehow, make sure we don't load it
