790466d055b7c706b88aed41905a31de48b450eb
Simon Baumgardt-Wellander <simonbw@uw.edu>
2021-04-24 14:14:51 -0700

    randomize jellyfish animation start and speed

--- src/mariana/enemies/Jellyfish.ts
+++ src/mariana/enemies/Jellyfish.ts
@@ -1,5 +1,6 @@
 import { Body, Circle } from "p2";
 import { AnimatedSprite, Sprite, Texture } from "pixi.js";
+import { runInContext } from "vm";
 import img_jellyfish from "../../../resources/images/jellyfish.png";
 import img_jellyfish1 from "../../../resources/images/jellyfish_1.png";
 import img_jellyfish2 from "../../../resources/images/jellyfish_2.png";
@@ -12,22 +13,23 @@ import { Diver } from "../Diver";
 export class Jellyfish extends BaseEntity implements Entity {
   sprite: AnimatedSprite & GameSprite;
 
-  constructor(position: V2d, radius: number = rUniform(0.3, 0.9)) {
+  constructor(position: V2d, radius: number = rUniform(0.4, 0.9)) {
     super();
 
     this.body = new Body({ mass: 0, collisionResponse: false });
     this.body.addShape(new Circle({ radius }));
     this.body.position = position;
 
-    this.sprite = new AnimatedSprite(
-      [Texture.from(img_jellyfish1), Texture.from(img_jellyfish2)],
-      false
-    );
+    this.sprite = AnimatedSprite.fromImages([img_jellyfish1, img_jellyfish2]);
+    this.sprite.animationSpeed = rUniform(0.9, 1.2);
+    this.sprite.autoUpdate = false;
     this.sprite.width = radius * 2;
     this.sprite.height = radius * 2;
     this.sprite.anchor.set(0.5);
     this.sprite.loop = true;
     this.sprite.position.set(...position);
+
+    this.sprite.update(Math.random() * 1.0);
   }
 
   onRender(dt: number) {
