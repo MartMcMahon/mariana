434c268bc1bcd8a08692c2c56c2b0f4f8e5072c2
Mart <themcman@gmail.com>
2021-04-24 00:24:31 -0500

    added the diver image

--- src/mariana/preloader/preloadImages.ts
+++ src/mariana/preloader/preloadImages.ts
@@ -1,7 +1,9 @@
+import img_diver from "../../../resources/images/diver.png";
+
 // Returns the list of all
 export function getImagesToPreload(): Set<string> {
   // use a set to make sure we don't include stuff multiple times
-  const urls = new Set([]);
+  const urls = new Set([img_diver]);
 
   // Just in case this sneaks in there somehow, make sure we don't load it
   urls.delete(undefined!);
