c40125681b8514e99df57f04db159657f1720455
Simon Baumgardt-Wellander <simonbw@uw.edu>
2021-05-03 20:01:27 -0700

    use simplex noise instead of uniform

--- src/mariana/world/worldGen.ts
+++ src/mariana/world/worldGen.ts
@@ -1,14 +1,19 @@
+import { makeNoise2D } from "fast-simplex-noise";
 import Grid from "../../core/util/Grid";
-import { rInteger } from "../../core/util/Random";
+import { lerp } from "../../core/util/MathUtil";
 import { WORLD_SIZE_TILES } from "../constants";
 
 export function generateSolidMap(): Grid<boolean> {
   const solidMap: Grid<boolean> = new Grid();
 
+  // const noise = makeNoise2D();
+  const noise = makeNoise2D();
+
   const minX = Math.floor(-WORLD_SIZE_TILES[0] / 2);
   const maxX = Math.floor(WORLD_SIZE_TILES[0] / 2);
   for (let x = minX; x < maxX; x++) {
-    let minY = rInteger(10, 30);
+    const n = (noise(x * 0.1, 0) + 1) / 2;
+    let minY = Math.floor(lerp(10, 40, n));
     const maxY = 100;
     for (let y = minY; y < maxY; y++) {
       solidMap.set([x, y], true);
